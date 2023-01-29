1c688b1c0c0905588c5ff43c061f1b885236244c
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2023-01-29 09:11:18 -0800

    3rd Commit, small code/file cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
 import Pokemon from './components/Pokemon';
 import SinglePokemon from './components/SinglePokemon';
 
 function App() {
-  return (
-    <div className="container-fluid justify-content-between">
-      <div className="d-flex justify-content-between">
-        <Pokemon />
-        <SinglePokemon />
+   return (
+      <div className="container-fluid justify-content-between">
+         <div className="d-flex justify-content-between">
+            <Pokemon />
+            <SinglePokemon />
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default App;
