c82615e8274ef15723e398a56474dd35e55b386f
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2023-01-29 09:10:19 -0800

    2nd Commit, small code/file cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 import Pokemon from './components/Pokemon';
 
 function App() {
-  return (
-    <div className="container-fluid">
-      <Pokemon />
-    </div>
-  );
+   return (
+      <div className="container-fluid">
+         <Pokemon />
+      </div>
+   );
 }
 
 export default App;
